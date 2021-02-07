b9bdccd9169d9f625197f8d6396d32ad9d86ff4b
unknown <anwarhosen575@gmail.com>
2021-02-07 10:00:31 +0600

    first commit1

---
+++ letconst.js
@@ -0,0 +1,14 @@
+const hubby = "APM";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+const nayok = {name:"Salman Khan",phone:4242};
+
+// console.log(numbers);
+// console.log(nayok);
+
+let patientName = "Rahim Chacha";
+patientName ="fatema khala";
+console.log(patientName);
