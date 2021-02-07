c9f5215eb0a7fc62efe798dccd730702ed8c1497
unknown <anwarhosen575@gmail.com>
2021-02-07 11:25:08 +0600

    second

---
+++ template.js
@@ -0,0 +1,9 @@
+const firstName ="Justin";
+const lastName ="TimberLake";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+
+const multiline = `I am a student
+ I love my work
+ I hate u`
+console.log(multiline);
